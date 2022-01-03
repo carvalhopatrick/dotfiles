e7d09f9efbd061b5a8e2d19e3b31302a658a6f79
carvalhopatrick <thick98@gmail.com>
2022-01-02 23:01:16 -0300

    fix install script

--- install-wsl.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-stow wsl -t $HOME 
-
-# after cloning the dotfiles:
-# git submodule update --init --recursive
-
-# anytime I want to update my submodules
-# git submodule update --recursive --remote
