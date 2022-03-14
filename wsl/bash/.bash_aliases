e64109ddfc12ab4e2793117e45ed2445001c648e
ptrckpc <ptrckpc@motorola.com>
2022-03-14 14:41:21 -0300

    eld

---
+++ wsl/bash/.bash_aliases
@@ -0,0 +1,10 @@
+function cdl(){
+	cd $1 && ll
+}
+alias a_downloads="cd /mnt/c/Users/patrick.carvalho/Downloads"
+alias a_desktop="cd /mnt/c/Users/patrick.carvalho/Desktop"
+
+alias n++="/mnt/c/Program\ Files/Notepad++/Notepad++.exe"
+
+alias mserver="ssh 10.181.194.162"
+
---
+++ wsl/install-wsl_no-submodules.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# init and update submodules after cloning the dotfiles:
+#git submodule update --init --recursive
+
+# create the stow links for every group
+for d in */ ; do
+    stow $d -t $HOME
+done
+
+
+# anytime I want to just update my submodules
+# git submodule update --recursive --remote
